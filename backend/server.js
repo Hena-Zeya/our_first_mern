71564fed441c5c17be796780685452ef01a7f47f
Heya Kwon <heyabird@gmail.com>
2020-04-24 17:29:44 -0700

    retest

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 // allows us to parse json
 app.use(express.json());
-app.use(express.static('public')) 
+app.use(express.static('../public')) 
 
 
 // 
